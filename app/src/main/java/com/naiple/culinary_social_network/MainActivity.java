33b7bb4843dc1c7eb9e19d135c27a713325263a2
NikitaNpl <totmyanin.nikita@gmail.com>
2023-03-04 11:22:20 +0300

    initial commit

---
+++ app/src/main/java/com/naiple/culinary_social_network/MainActivity.java
@@ -0,0 +1,14 @@
+package com.naiple.culinary_social_network;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
